42d8b080ec4ec0f5326e6dd67a0e8f75a3a3285c
ILYA <106765406+D15ND@users.noreply.github.com>
2025-03-24 23:29:25 +0300

    feat: add js code

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,4 @@
-alert ("are you here?");
+let cookies="this page used cookies";
+confirm (cookies.toUpperCase());
+console.log("количество символов =", cookies.length);
+console.log("количество букв =", cookies.replaceAll(' ', '').length);
